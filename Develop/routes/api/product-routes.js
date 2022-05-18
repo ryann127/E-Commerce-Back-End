569751092ab91a8512779ad61746ec5c2c020ff6
ryann127 <ryann.goldberg127@gmail.com>
2022-05-18 15:41:51 -0600

    worked on category and product API routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,13 +7,39 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then((products) => res.json(products))
+  .catch((err) => res.json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  Product.findOne({
+  where: {
+    id: req.params.id
+  },
+  include: [
+    Category,
+    {
+      model: Tag,
+      through: ProductTag
+    }
+  ],
+})
+})
+.then((category) => res.json(category))
+.catch((err) => res.json(err))
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -91,6 +117,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((product) => res.json(product))
+  .catch((err) => res.json(err))
 });
 
 module.exports = router;
