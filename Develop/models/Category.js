6a716645a21b027ee3c60486da4635b3cf46cf52
ryann127 <ryann.goldberg127@gmail.com>
2022-05-19 19:05:07 -0600

    got rid of some unnecessary curly brackets to get npm seed to work after a few debugging tries and ran npm run seed

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -12,9 +12,8 @@ Category.init(
       allowNull: false, 
       primaryKey: true,
       autoIncrement: true
-    }
-  },
-  {
+    },
+
     category_name: {
       type: DataTypes.STRING,
       allowNull: false
